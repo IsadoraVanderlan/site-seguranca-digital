030f76c604ea0297f20ae63cbe5dffe687edaa85
Isadora Vanderlan <vanderlansantos1991@gmail.com>
2021-11-03 14:53:42 -0300

    js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$("#navbar a").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
